2a44fb19ecfedae1a0cee8f74776e0e025cba411
asilfndk <fndkasil@gmail.com>
2022-12-18 00:13:50 +0300

    .

--- 2) Finger Counting/finger_counting.py
+++ 2) Finger Counting/finger_counting.py
@@ -4,41 +4,55 @@ import mediapipe as mp
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+
 mpHand = mp.solutions.hands
 hands = mpHand.Hands()
-mpDraw = mp.solutions.drawing_utils
-tipIds = [4, 8, 12, 16, 20]
+mpDraw = mp.solutions.drawing_utils 
 
+tipIds = [4, 8, 12, 16, 20]
 while True:
+    
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
     results = hands.process(imgRGB)
-
+    # print(results.multi_hand_landmarks)
+    
     lmList = []
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHand.HAND_CONNECTIONS)
-            for id, lm in enumerate(handLms.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append([id, cx, cy])
     
+            for id, lm in enumerate(handLms.landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmList.append([id, cx, cy])
+                              
+                # if id == 8:
+                #     cv2.circle(img, (cx,cy), 9, (255,0,0), cv2.FILLED)
+                # if id == 6:
+                #     cv2.circle(img, (cx,cy), 9, (0,0,255), cv2.FILLED)
+       
     if len(lmList) != 0:
         fingers = []
-
+               
         if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
-
+        
         for id in range(1, 5):
-            if lmList[tipIds[0]][1] < lmList[tipIds[0] - 2][2]:
+            
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
-
-        totalF = fingers.count(1)
-        cv2.putText(img, str(totalF), (30, 125), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)           
+            
+        # print(fingers)
     
-    cv2.imshow("img", img)
+        totalF = fingers.count(1)
+        # print(totalF)
+        cv2.putText(img, str(totalF), (30,125), cv2.FONT_HERSHEY_PLAIN, 10, (255,0,0),8)
+        
+    cv2.imshow("img",img)
     cv2.waitKey(1)
